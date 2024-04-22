7dd6b08a0054a565bdd414341a8d6833086e2fe8
Mustafa Khaleel ( MK ) <mkhaleel190@gmail.com>
2024-04-22 11:00:13 +0000

    update extra_packages.sh

--- scripts/extra_packages.sh
+++ scripts/extra_packages.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo -e "Install Extra Packages"
-sleep 1
-
-# pacman --noconfirm -S git unzip unrar firefox vlc file-roller
-
-sudo pacman -S git unzip unrar firefox vlc file-roller jdk-openjdk python
-
-
-
 echo -e "Intalling Fonts"
 sleep 1
 sudo pacman --noconfirm -S ttf-liberation ttf-bitstream-vera ttf-dejavu ttf-droid ttf-freefont noto-fonts adobe-source-code-pro-fonts ttf-ubuntu-font-family
+
+
+echo -e "Install Extra Packages"
+sleep 1
+
+sudo pacman --noconfirm -S git unzip unrar firefox vlc file-roller jdk-openjdk python python-pip
+pip install pip==22.3.1 --break-system-packages
+
+
+
